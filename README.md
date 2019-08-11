3c3d1229ea74808dc79fbf5ad547725085d6dcfd
Jeff Mery <jmery@users.noreply.github.com>
2019-08-11 11:44:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# python
+
+Forked from core/python to bump versions more often
+
+## Maintainers
+
+Jeff Mery (jmery@users.noreply.github.com)
+
+## Type of Package
+
+Binary package for python.
+
+## Usage
+
+Can be called as dependency and/or binlinked for local usage.
---
+++ plan.sh
@@ -0,0 +1,87 @@
+pkg_name=python
+pkg_distname=Python
+pkg_version=3.7.4
+pkg_origin=jmery
+pkg_maintainer="Jeff Mery <jmery@chef.io>"
+pkg_license=('Python-2.0')
+pkg_description="Python is a programming language that lets you work quickly \
+  and integrate systems more effectively."
+pkg_upstream_url="https://www.python.org"
+pkg_dirname="${pkg_distname}-${pkg_version}"
+pkg_source="https://www.python.org/ftp/python/${pkg_version}/${pkg_dirname}.tgz"
+pkg_shasum="d63e63e14e6d29e17490abbe6f7d17afb3db182dbd801229f14e55f4157c4ba3"
+
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_interpreters=(bin/python bin/python3 bin/python3.7)
+
+pkg_deps=(
+  core/bzip2
+  core/expat
+  core/gcc-libs
+  core/gdbm
+  core/glibc
+  core/libffi
+  core/ncurses
+  core/openssl
+  core/readline
+  core/sqlite
+  core/zlib
+)
+
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/gcc
+  core/linux-headers
+  core/make
+  core/util-linux
+)
+
+do_prepare() {
+  sed -i.bak 's/#zlib/zlib/' Modules/Setup.dist
+  sed -i -re "/(SSL=|_ssl|-DUSE_SSL|-lssl).*/ s|^#||" Modules/Setup.dist
+}
+
+do_build() {
+  export LDFLAGS="$LDFLAGS -lgcc_s"
+
+  # Should add --enable-optimizations build flag at some point
+  # Increases compiled performance @ expense of compile time
+  ./configure --prefix="$pkg_prefix" \
+              --enable-loadable-sqlite-extensions \
+              --enable-shared \
+              --with-threads \
+              --with-system-expat \
+              --with-system-ffi \
+              --with-ensurepip
+
+  make
+}
+
+do_check() {
+  make test
+}
+
+do_install() {
+  do_default_install
+
+  # link pythonx.x to python for pkg_interpreters
+  local minor=${pkg_version%.*}
+  local major=${minor%.*}
+  ln -rs "$pkg_prefix/bin/pip$minor" "$pkg_prefix/bin/pip"
+  ln -rs "$pkg_prefix/bin/pydoc$minor" "$pkg_prefix/bin/pydoc"
+  ln -rs "$pkg_prefix/bin/python$minor" "$pkg_prefix/bin/python"
+  ln -rs "$pkg_prefix/bin/python$minor-config" "$pkg_prefix/bin/python-config"
+
+  # Remove idle as we are not building with Tk/x11 support so it is useless
+  rm -vf "$pkg_prefix/bin/idle$major"
+  rm -vf "$pkg_prefix/bin/idle$minor"
+
+  platlib=$(python -c "import sysconfig;print(sysconfig.get_path('platlib'))")
+  cat <<EOF > "$platlib/_manylinux.py"
+# Disable binary manylinux1(CentOS 5) wheel support
+manylinux1_compatible = False
+EOF
+}
